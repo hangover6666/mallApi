6e3395969f05fa3fa5691a8eb6dfcae64610cfbd
KG <11195047+kuang-guokeng@user.noreply.gitee.com>
2025-04-14 17:22:30 +0800

    获取用户个人信息FINISH

--- src/test/java/com/zust/mallapi/MallApiApplicationTests.java
+++ src/test/java/com/zust/mallapi/MallApiApplicationTests.java
@@ -3,11 +3,14 @@ package com.zust.mallapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.UUID;
+
 @SpringBootTest
 class MallApiApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println(UUID.randomUUID());
     }
 
 }
